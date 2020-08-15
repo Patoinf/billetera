4adcccd9acf33685c2e1912ba718acf0f3795deb
Patoinf <patoinf@gmail.com>
2020-08-14 20:37:17 -0400

    1mer commit

--- billeteragit.py
+++ billeteragit.py
@@ -10,7 +10,7 @@ from datetime import datetime
 monedasCMC = dict()
 monedasBilletera = dict()
 transacciones = list()
-direccionBilletera = "HELL"
+direccionBilletera = "HELLO"
 
 # Imprime la lista de Comandos
 def imprimirAyuda():
